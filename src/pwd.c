3a7eb383b04a833cb59951973249d016723af533
mehmedaltug <mehmed.ede@hotmail.com>
2026-09-13 15:49:48 +0300

    implemented pwd

---
+++ src/pwd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <limits.h>
+
+#ifdef _WIN32
+    #include <direct.h>
+    #define getcwd _getcwd
+#else
+    #include <unistd.h
+#endif
+
+int main (void) {
+    char cwd[PATH_MAX];
+    
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+        printf("%s\n", cwd);
+    else {
+        perror("Could not get CWD");
+        return 1;
+    }
+    return 0;
+}
