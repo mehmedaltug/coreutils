2c5519e76b4ccff2a17985758ae15537587e0a33
mehmedaltug <mehmed.ede@hotmail.com>
2026-09-19 09:27:31 +0300

    removed unused function

--- lib/regex.h
+++ lib/regex.h
@@ -63,41 +63,6 @@ int regcomp(regex_t *, const char *, int);
 int regexec(const regex_t *, const char *, size_t, regmatch_t *, int);
 void regfree(regex_t *);
 
-size_t regerror(int, const regex_t *, char *, size_t);
-
-/* From regerror.c */
-
-/* Error message strings for error codes listed in `regex.h'.  This list
-   needs to be in sync with the codes listed there, naturally. */
-
-/* Converted to single string by Rich Felker to remove the need for
- * data relocations at runtime, 27 Feb 2006. */
-
-static const char musl_regex_messages[] = {
-    "No error\0"
-    "No match\0"
-    "Invalid regexp\0"
-    "Unknown collating element\0"
-    "Unknown character class name\0"
-    "Trailing backslash\0"
-    "Invalid back reference\0"
-    "Missing ']'\0"
-    "Missing ')'\0"
-    "Missing '}'\0"
-    "Invalid contents of {}\0"
-    "Invalid character range\0"
-    "Out of memory\0"
-    "Repetition not preceded by valid expression\0"
-    "\0Unknown error"};
-
-inline size_t regerror(int e, const regex_t *preg, char *buf, size_t size) {
-  const char *s;
-  for (s = musl_regex_messages; e && *s; e--, s += strlen(s) + 1)
-    ;
-  if (!*s) s++;
-  return 1 + snprintf(buf, size, "%s", s);
-}
-
 /*
   tre-internal.h - TRE internal definitions
 
